1950f35e539884c2cfb464029347b9904510e358
hp <sabhrwalkanika1999@gmail.com>
2023-10-10 22:53:31 +0530

    Add project files.

---
+++ Generic Collection/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Collection
+{
+    internal class MyDictionary
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string,string> d = new Dictionary<string,string>();
+            Console.WriteLine("Synonyms:");
+            d.Add("Admit", "Confess");
+            d.Add("reply", "answer");
+            d.Add("Thief" , "Burglar");
+            d.Add("Rich", "Wealthy");
+            d.Add("Weeping", "Crying");
+
+            foreach(KeyValuePair<string,string> kvp in d)
+            {
+                Console.WriteLine(kvp.Key + " : " + kvp.Value );
+            }
+
+            foreach(var i in d.Keys)
+            {
+                Console.WriteLine(i);
+            }
+            foreach(var i in d.Values)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Generic Collection/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("Hello");
+            list.Add("How");
+            list.Add("Are");
+            list.Add("You");
+
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+            //Console.WriteLine(list[0]);
+
+            List<int> list2 = new List<int>();
+            list2.Add(1);
+            list2.Add(2);
+            list2.Add(3);
+            list2.Add(4);
+            list2.Add(5);
+            list2.Add(6);
+            list2.Add(6);
+
+            list2.Reverse();
+            Console.WriteLine(list2.IndexOf(6));
+            Console.WriteLine(list2.LastIndexOf(6));
+
+            foreach (int item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+            Employee e1 = new Employee()
+            {
+                name = "Kanika",
+                age = 24
+            };
+            Employee e2 = new Employee()
+            {
+                name = "Deevanshu",
+                age = 21
+            };
+
+
+            List<Employee> list3 = new List<Employee>();
+            list3.Add(e1);
+            list3.Add(e2);
+
+            foreach (Employee e3 in list3)
+            {
+                Console.WriteLine($"Name: {e3.name}, Age: {e3.age}");
+            }
+
+            Console.WriteLine(list3.Exists(e3 => e3.name.StartsWith("D")));
+
+            Employee e = list3.Find(e3 => e3.age < 22);
+            Console.WriteLine("The age less than 22 is of " + e.name);
+
+            // similarly FindLast() is used to find last occurence.
+            // And FindAll() gives all the occurences.
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
